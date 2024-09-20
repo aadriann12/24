5f55abe4d0e654f9ed0258e8512eb630437a8973
aadriann12 <aterans03@educantabria.es>
2024-09-20 16:22:13 +0200

    ejercicios prog

---
+++ tema1/and_y_eso/src/main/java/com/mycompany/and_y_eso/And_y_eso.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.and_y_eso;
+
+/**
+ *
+ * @author DAW109
+ */
+public class And_y_eso {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
